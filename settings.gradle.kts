c13885ab62271bc73b9323018e1ae36a942a61e5
Tucker Trainor <tuckertrainor@users.noreply.github.com>
2026-06-05 11:30:11 -0400

    build: define function within each block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,42 +1,37 @@
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
-fun getMavenCredentials(): Pair<String, String>? {
-    val userHome = System.getProperty("user.home")
-    val mavenSettingsFile = File(userHome, ".m2/settings.xml")
-    if (!mavenSettingsFile.exists()) {
+pluginManagement {
+    fun getMavenCredentials(): Pair<String, String>? {
+        val userHome = System.getProperty("user.home")
+        val mavenSettingsFile = File(userHome, ".m2/settings.xml")
+        if (!mavenSettingsFile.exists()) {
+            return null
+        }
+        val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        val document = documentBuilder.parse(mavenSettingsFile)
+        val servers = document.getElementsByTagName("server")
+        for (i in 0 until servers.length) {
+            val serverNode = servers.item(i)
+            val children = serverNode.childNodes
+            var id: String? = null
+            var username: String? = null
+            var password: String? = null
+            for (j in 0 until children.length) {
+                val node = children.item(j)
+                when (node.nodeName) {
+                    "id" -> id = node.textContent
+                    "username" -> username = node.textContent
+                    "password" -> password = node.textContent
+                }
+            }
+            if (id == "HubSpot-Nexus" && username != null && password != null) {
+                return Pair(username, password)
+            }
+        }
         return null
     }
 
-    val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-    val document = documentBuilder.parse(mavenSettingsFile)
-
-    val servers = document.getElementsByTagName("server")
-    for (i in 0 until servers.length) {
-        val serverNode = servers.item(i)
-        val children = serverNode.childNodes
-        var id: String? = null
-        var username: String? = null
-        var password: String? = null
-
-        for (j in 0 until children.length) {
-            val node = children.item(j)
-            when (node.nodeName) {
-                "id" -> id = node.textContent
-                "username" -> username = node.textContent
-                "password" -> password = node.textContent
-            }
-        }
-
-        if (id == "HubSpot-Nexus" && username != null && password != null) {
-            return Pair(username, password)
-        }
-    }
-
-    return null
-}
-
-pluginManagement {
     repositories {
         if (System.getenv("BLAZAR_COORDINATES") != null) {
             maven("https://nexus.hubteam.com/nexus-maven/repository/hubspot-development/") {
@@ -54,8 +49,40 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    fun getMavenCredentials(): Pair<String, String>? {
+        val userHome = System.getProperty("user.home")
+        val mavenSettingsFile = File(userHome, ".m2/settings.xml")
+        if (!mavenSettingsFile.exists()) {
+            return null
+        }
+        val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        val document = documentBuilder.parse(mavenSettingsFile)
+        val servers = document.getElementsByTagName("server")
+        for (i in 0 until servers.length) {
+            val serverNode = servers.item(i)
+            val children = serverNode.childNodes
+            var id: String? = null
+            var username: String? = null
+            var password: String? = null
+            for (j in 0 until children.length) {
+                val node = children.item(j)
+                when (node.nodeName) {
+                    "id" -> id = node.textContent
+                    "username" -> username = node.textContent
+                    "password" -> password = node.textContent
+                }
+            }
+            if (id == "HubSpot-Nexus" && username != null && password != null) {
+                return Pair(username, password)
+            }
+        }
+        return null
+    }
+
     repositories {
         if (System.getenv("BLAZAR_COORDINATES") != null) {
             maven("https://nexus.hubteam.com/nexus-maven/repository/hubspot-development/") {
